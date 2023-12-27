cea720799c8bae18fdd4cfea41184761090c09de
0xKoda <88812262+0xKoda@users.noreply.github.com>
2023-12-28 09:26:48 +1000

    Ready for deployment

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# laundry
+# Laundry
+
+Born from [Storm Chasers](t.me/chainchasers) and [Gunwatch](t.me/railgunwatch), Laundry is tool for tracking and analyzing consolidated withdrawals from privacy-centric platforms such as Tornado Cash and Railgun. Threat actors use services like Tornado Cash and Railgun to move funds and obfuscate the trail.  Tools like laundry, railgunwatch and storm chasers are specifically crafted to augment the efficiency of tracing activities, bridging the gap in rapid transaction monitoring and sleuthing, thereby countering the agility of Threat Actors. Laundry, alongside Storm Chasers and Gunwatch, represents a commitment to advancing the tracing toolkit in the cryptocurrency realm.
+
+
+## Key Features
+
+- **Consolidated Data Display**: Laundry consolidates wihdrawals from Railgun and Tornado, and displays them in a digestible way.
+  
+- **Efficient Search Functionality**: One of the core functionalities of Laundry is its ability to facilitate efficient searching of addresses for matches. This feature is a significant improvement over the existing methods used on platforms like Etherscan, making the process less cumbersome and more user-friendly.
+
+- **Focused Dataset**: Laundry's dataset is stored, beginning from December 27th, the date when it started storing transaction data. Sotring the data it scrapes facilitates faster searches.
+
+## For Historical Data or Alerts
+
+For those interested in more historical data beyond Laundry's scope, it's recommended to refer to the original Telegram channels, [Storm Chasers](t.me/chainchasers) and [Gunwatch](t.me/railgunwatch). These channels provide a wealth of information and are great resources for deeper historical insights.
+
+## Upcoming Features
+
+- **Railgun on Arbitrum**
+- **Bitcoin Mixers**
+- **ThorSwap**
+
+Laundry is committed to improving the current stack for hunting threat actors across mixers. 
--- script.js
+++ script.js
@@ -1,59 +1,65 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Load the last two transactions as placeholders
-  fetch('ledger.json')
-    .then(response => response.json())
-    .then(data => {
-      const transactions = Object.values(data);
-      displayTransactions(transactions.slice(-2)); // Display the last 2 transactions
-    });
-
-  // Enable the search input and button
+  loadInitialTransactions();
   document.getElementById('search-input').disabled = false;
   document.getElementById('search-button').disabled = false;
 
-  // Event listener for the search button
-  document.getElementById('search-button').addEventListener('click', function() {
+
+  document.getElementById('search-button').addEventListener('click', performSearch);
+
+
+  document.getElementById('search-input').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+      performSearch();
+    }
+  });
+
+  function performSearch() {
     const address = document.getElementById('search-input').value.trim();
     if (address) {
       searchTransactions(address);
     }
-  });
+  }
 
-  // Function to display transactions in the results box
   function displayTransactions(transactions) {
     const resultsElement = document.getElementById('results');
-    resultsElement.innerHTML = ''; // Clear previous results
+    resultsElement.innerHTML = '';
 
-    transactions.forEach(transaction => {
+    transactions.forEach(([txHash, transaction]) => {
       const transactionElement = document.createElement('div');
       transactionElement.classList.add('transaction');
+      const dateOnly = transaction.date.split('T')[0];
+
       transactionElement.innerHTML = `
-        <div>Date: ${transaction.date}</div>
+        <div>Tx Hash: <a href="https://etherscan.io/tx/${txHash}" target="_blank">${txHash}</a></div>
+        <div>Date: ${dateOnly}</div>
         <div>Amount: ${transaction.amount} ${transaction.asset}</div>
         <div>Protocol: ${transaction.protocol}</div>
         <div>From: ${transaction.fromAddress}</div>
-        <div>To: ${transaction.toAddress}</div>
+        <div>To: <a href="https://metasleuth.io/result/eth/${transaction.toAddress}" target="_blank">${transaction.toAddress}</a></div>
       `;
       resultsElement.appendChild(transactionElement);
     });
   }
 
-  // Search function to find transactions that match the entered address
+  function loadInitialTransactions() {
+    fetch('ledger.json')
+      .then(response => response.json())
+      .then(data => {
+        const transactions = Object.entries(data);
+        displayTransactions(transactions.slice(-2));
+      });
+  }
+
   function searchTransactions(address) {
     fetch('ledger.json')
       .then(response => response.json())
       .then(data => {
-        const transactions = Object.values(data);
-        const matchedTransactions = transactions.filter(transaction =>
+        const transactions = Object.entries(data);
+        const matchedTransactions = transactions.filter(([txHash, transaction]) =>
           transaction.fromAddress.toLowerCase() === address.toLowerCase() ||
           transaction.toAddress.toLowerCase() === address.toLowerCase()
         );
-        if (matchedTransactions.length === 0) {
-          const resultsElement = document.getElementById('results');
-          resultsElement.innerHTML = '<div>No transactions found.</div>';
-        } else {
-          displayTransactions(matchedTransactions);
-        }
+        displayTransactions(matchedTransactions);
       })
       .catch(error => {
         console.error('Error fetching the transactions:', error);
